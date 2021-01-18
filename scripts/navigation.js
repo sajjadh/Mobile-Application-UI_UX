852110c9fdbf049d620b6df79e801d8377074871
Maleesh Piyaratne <maleeshpiyaratne@gmail.com>
2021-01-18 11:41:47 +0530

    Navigation for Accounts - back to pages

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -35,4 +35,38 @@ $(document).ready(function() {
 		window.location.href = 'test.html';	
 	})
 
+	// go to Settings
+	$('.settings').on('click', function() { 
+		window.location.href = 'settings.html';	
+	})
+
+	// go to Payment Methods
+	$('.payment_method').on('click', function() { 
+		window.location.href = 'payment_method.html';	
+	})
+	// go to Rewards
+	$('.rewards').on('click', function() { 
+		window.location.href = 'rewards.html';	
+	})
+
+	// go to Orders
+	$('.orders').on('click', function() { 
+		window.location.href = 'orders.html';	
+	})
+
+	// go to Location
+	$('.location').on('click', function() { 
+		window.location.href = 'location.html';	
+	})
+
+	// go to Invite Friends
+	$('.invite').on('click', function() { 
+		window.location.href = 'test.html';	
+	})
+
+	// go to Invite Friends
+	$('.edit_profile').on('click', function() { 
+		window.location.href = 'edit_profile.html';	
+	})
+	
 });
